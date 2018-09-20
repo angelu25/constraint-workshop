c79f8d0d04ae594a68aa52300de8873c33dcdc53
Jose Angel Zamora <joseangel.zamora@bestsecret.com>
2018-09-20 17:27:47 +0200

    Exercise 2 final version

--- app/src/main/java/com/bestsecret/constraintlayout/MainActivity.kt
+++ app/src/main/java/com/bestsecret/constraintlayout/MainActivity.kt
@@ -2,14 +2,12 @@ package com.bestsecret.constraintlayout
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
-        setSupportActionBar(toolbar)
     }
 
 }
