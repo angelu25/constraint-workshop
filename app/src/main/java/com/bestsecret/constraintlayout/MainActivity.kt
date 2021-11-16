76498da9dffc52a1ad98669f0a94165cb9433c34
Jose Angel Zamora <joseangel.zamora@schustermann-borenstein.de>
2021-11-16 15:47:17 +0100

    Add scene with motion

--- app/src/main/java/com/bestsecret/constraintlayout/MainActivity.kt
+++ app/src/main/java/com/bestsecret/constraintlayout/MainActivity.kt
@@ -9,32 +9,9 @@ import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
-    private var isLargeLayout = false
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val constraintSet1 = ConstraintSet()
-        val constraintSet2 = ConstraintSet()
-
-        constraintSet2.clone(this, R.layout.main_large)
         setContentView(R.layout.main)
-
-        val constraintLayout = findViewById<ConstraintLayout>(R.id.constraint_layout)
-        constraintSet1.clone(constraintLayout)
-
-        val posterImageView = findViewById<ImageView>(R.id.imageview_poster)
-
-        posterImageView.setOnClickListener {
-
-            TransitionManager.beginDelayedTransition(constraintLayout)
-            if (isLargeLayout) {
-                constraintSet1.applyTo(constraintLayout)
-            } else {
-                constraintSet2.applyTo(constraintLayout)
-            }
-            isLargeLayout = !isLargeLayout
-        }
     }
 
 }
